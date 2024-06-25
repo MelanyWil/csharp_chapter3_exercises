e2587aacb97a63ef99f93f6851dabfe0c70582ce
Melany Willis <melexiw@gmail.com>
2024-06-25 11:03:07 -0500

    added strings exercise

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Arrays!");
 int[] numbers = {1, 1, 2, 3, 5, 8};
 foreach(int number in numbers) {
     if(number % 2 == 0) {
---
+++ Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Strings!");
+
+string suess = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+string[] suessArray = suess.Split(".");
+Console.WriteLine(string.Join(",", suessArray));
